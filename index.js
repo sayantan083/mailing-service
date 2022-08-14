8f843e33bdd3d43adec46bf7e2ccb0efaf9fc572
Sayantan Panda <sayantan@goodmeetings.ai>
2022-08-14 13:50:27 +0530

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = require('./router/router')
 const task = require('./helpers/cronScheduler')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static("public"))
 app.use(express.json());
